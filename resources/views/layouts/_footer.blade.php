2211d1c07fe58a41f7e18bf15eeb0b9666dd0115
kuro <kuroneko96@126.com>
2022-12-29 11:43:37 +0800

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <img src="https://iocaffcdn.phphub.org/uploads/sites/KDiyAbV0hj" alt="" class="brand-icon">
+    <a href="https://laravel-china.org/courses" target="_blank">
+        刻意练习，每日精进
+    </a>
+    <div class="float-right">
+        <a href="/about">关于</a>
+    </div>
+</footer>
