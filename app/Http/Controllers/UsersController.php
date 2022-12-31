05e3be884e2e22718ad92f104e67780dfc2a67ba
kuro <kuroneko961718@gmail.com>
2022-12-31 17:27:39 +0800

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -69,4 +69,37 @@ class UsersController extends Controller
         session()->flash('success', '欢迎，您将在这里开启一段新的路程~');
         return redirect()->route('users.show', [$user]);
     }
+    
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user,Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            // 修改字段验证的值可为nullable，当字段值为空时可以不用检验
+            // 'password' => 'required|confirmed|min:6',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        // 根据用户输入的字段自动判定是否需要修改对应的字段
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+        
+        /* 
+        // 手动更新用户输入的字段值
+        $user->update([
+            'name' => $request->name,
+            'password' => bcrypt($request->password)
+        ]);
+         */
+        session()->flash('success', '个人资料更新成功！');
+        return redirect()->route('users.show', $user);
+    }
 }
