cdec537538d73b0c720f2e90a2228e0adc233461
kuro <kuroneko961718@gmail.com>
2023-01-05 21:40:49 +0800

    用户发表微博动态

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -7,6 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Status extends Model
 {
     /**
+     * 允许用户提交信息修改的status模型的字段
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'content'
+    ];
+
+    /**
      * 关联user模型
      * 指明status模型是user模型的从属
      *
